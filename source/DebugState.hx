5899138accc1d6d150a552a59c6fb4e3d00faf61
kevinp15 <KevinDevel3@gmail.com>
2021-07-11 21:05:21 -0400

    Added stepmania section and got reading files from that directory working

--- source/DebugState.hx
+++ source/DebugState.hx
@@ -1,6 +1,7 @@
 //ShowStatState
 package;
 
+import sys.io.File;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -32,61 +33,24 @@ using StringTools;
 
 class DebugState extends MusicBeatState
 {
-    var testSprite:FlxSprite;
-    var upperSprite:FlxSprite;
-    var amount:Float = 120.0;
-
-    var scaletext:FlxText;
-    var heighttext:FlxText;
-
     public function new()
     {
+        #if sys
+        trace("you can read SM files!");
+        #end
+
         super();
     }
 
 	override public function create():Void
 	{
+        var SMContent = File.getContent("assets/stepmania/grass-skirt-crowdkill/grass-skirt-crowdkill.sm");
 		super.create();
-
-        testSprite = new FlxSprite(0, 0);
-        testSprite.frames = Paths.getSparrowAtlas('NOTE_assets', 'shared');
-        testSprite.animation.addByPrefix('piece', 'green hold piece');
-        testSprite.animation.play('piece');
-        add(testSprite);
-        testSprite.screenCenter();
-
-        upperSprite = new FlxSprite(0, 0);
-        upperSprite.frames = Paths.getSparrowAtlas('NOTE_assets', 'shared');
-        upperSprite.animation.addByPrefix('end', 'green hold end');
-        upperSprite.animation.play('end');
-        add(upperSprite);
-        upperSprite.screenCenter();
-
-        scaletext = new FlxText(0, 0, 0, "");
-        add(scaletext);
-
-        heighttext = new FlxText(0, 10, 0, "");
-        add(heighttext);
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
-        if(FlxG.keys.pressed.ESCAPE)
-            FlxG.switchState(new TitleState());
-        if(FlxG.keys.pressed.UP)
-            upperSprite.y += amount * elapsed;
-        if(FlxG.keys.pressed.DOWN)
-            upperSprite.y -= amount * elapsed;
-        testSprite.scale.y = upperSprite.y - testSprite.y;
-        testSprite.screenCenter();
-        testSprite.y = FlxG.height / 2;
-        testSprite.updateHitbox();
-
-        upperSprite.x = testSprite.x;
-
-        scaletext.text = "SCALE: " + testSprite.scale.y;
-        heighttext.text = "HEIGHT: " + testSprite.height;
 	}
 
 	function truncateFloat( number : Float, precision : Int): Float 
