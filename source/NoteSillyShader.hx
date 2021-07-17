ac86750379f12d969f22be04ad7f5bc620739703
kevinp15 <KevinDevel3@gmail.com>
2021-07-17 00:44:29 -0400

    Oh my god I fucking suck at coding

--- source/NoteSillyShader.hx
+++ source/NoteSillyShader.hx
@@ -14,8 +14,8 @@ class NoteSillyShader extends FlxShader
 
     void main()
     {
-        openfl_TextureCoordv.x += 0.05 * coefficient * sin(12.56637061 * (openfl_TextureCoordv.y + time / 5.0));
         vec2 coords = openfl_TextureCoordv;
+        coords.x += 0.05 * coefficient * sin(12.56637061 * (openfl_TextureCoordv.y + time / 5.0));
         vec4 color = flixel_texture2D(bitmap, coords);
         
         gl_FragColor = vec4(color.r, color.g, color.b, color.a);
